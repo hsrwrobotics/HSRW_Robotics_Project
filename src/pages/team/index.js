07fb6eb5548139776d9c2d7939060a8eda55166b
mtc-20 <mamenthomas.c@gmail.com>
2021-06-22 18:28:32 +0200

    more edits and missing files

--- src/pages/team/index.js
+++ src/pages/team/index.js
@@ -1,34 +1,54 @@
 import React, { useState } from 'react';
 import Layout from '@theme/Layout';
 import styles from "./index.module.css"
-import {Link} from 'react-router-dom'
+
+
+
 
 const ContactCard = props => {
   return(
     <div className={styles.contactcard}>
       <img src={props.avatar} alt='profile' style={{width:'20vh'}}/>
       <p>{props.name}</p>
-      <p> <Link to='https://github.com/mtc-20'>@{props.github}</Link>
-      </p>
+      <p><a href={'https://github.com/'+props.github} target='_blank'>@{props.github}</a></p>
     </div>
   );
 };
 
 // Just sample data
 const contacts = [
-  {name: 'Mamen Thomas Chembakasseril', github:'mtc-20', avatar:'https://avatars.githubusercontent.com/u/50070186?v=4'},
-  {name: 'ABD', github:'mas'},
+  {name: 'Mamen Thomas Chembakasseril', github:'mtc-20', ghlink:'https://github.com/mtc-20', avatar:'https://avatars.githubusercontent.com/u/50070186?v=4'},
+  {name: 'Quang', github:'quangnhat185',avatar:'https://avatars.githubusercontent.com/u/41325216?v=4'},
+  {name: 'ABD', github:'hsrwrobotics'},
+  {name: 'ABD', github:'hsrwrobotics'},
+  {name: 'ABD', github:'hsrwrobotics'},
+  {name: 'ABD', github:'hsrwrobotics'},
+  {name: 'ABD', github:'hsrwrobotics'},
+  {name: 'ABD', github:'hsrwrobotics'},
+  {name: 'ABD', github:'hsrwrobotics'},
+  {name: 'ABD', github:'hsrwrobotics'},
+  {name: 'ABD', github:'hsrwrobotics'},
+  {name: 'ABD', github:'hsrwrobotics'},
+  {name: 'ABD', github:'hsrwrobotics'},
+  {name: 'ABD', github:'hsrwrobotics'},
+  {name: 'ABD', github:'hsrwrobotics'},
+  {name: 'ABD', github:'hsrwrobotics'},
+  {name: 'ABD', github:'hsrwrobotics'},
+  {name: 'ABD', github:'hsrwrobotics'},
+  {name: 'ABD', github:'hsrwrobotics'},
+  {name: 'ABD', github:'hsrwrobotics'},
+  {name: 'ABD', github:'hsrwrobotics'},
+  {name: 'ABD', github:'hsrwrobotics'},
+  {name: 'ABD', github:'hsrwrobotics'},
 
 ]
 
-const Form = props  => {
-  const [username, setUserName] = useState('')
-  handleSubmit = event => {
-    event.preventDefault()
-    axios 
-  }
-}
+
+
+
 function Hello() {
+
+  
   return (
     <Layout title="HSRW Robotics Club">
       <h1>About the Club</h1>
@@ -36,18 +56,25 @@ function Hello() {
       <div className={styles.content}>
       <p>
       Started in the summer of 2019 by a band of misfits.
+
+      Pick
+      <a href='https://github.com/hsrwrobotics'>A</a>
+      over and out.
       </p>
       </div>
 
 
       <h1>The Team</h1>
+      <div className={styles.cardContainer}>
+
       {contacts.map(contact=>(
         <ContactCard
         avatar = {contact.avatar}
         name = {contact.name}
         github={contact.github}
         />
-      ))}
+        ))}
+      </div>
       
     </Layout>
   );
