b71c764f7db853d781f0ac2abace414a439e438d
mtc-20 <mamenthomas.c@gmail.com>
2021-06-23 12:45:07 +0200

    v minor cleaning

--- src/pages/team/index.js
+++ src/pages/team/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Layout from '@theme/Layout';
 import styles from "./index.module.css"
 import data from "../../components/data/team.json"
@@ -15,60 +15,20 @@ const ContactCard = props => {
   );
 };
 
-// Just sample data
-// const contacts = [
-//   {name: 'Mamen Thomas Chembakasseril', login:'mtc-20', avatar_url:'https://avatars.githubusercontent.com/u/50070186?v=4'},
-//   {name: 'Quang', login:'quangnhat185', avatar_url:'https://avatars.githubusercontent.com/u/41325216?v=4'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-//   {name: 'ABD', login:'hsrwrobotics'},
-
-// ]
-
-// const cotacts = Array.from(data).map(contact=>{})
-
-// fetch(".\team.json")
-// .then(res=>{return res.json();})
-// .then(res=>{console.log(res)})
-// .catch(err=>{console.error(err)})
 
 
 function Hello() {
-  // const { siteConfig } = useDocusaurusContext();
 
-  
   return (
     <Layout title="HSRW Robotics Club">
       <h1>About the Club</h1>
 
       <div className={styles.content}>
       <p>
-      Started in the summer of 2019 by a band of misfits.
-
-      Pick
-      <a href='https://github.com/hsrwrobotics'>A</a>
-      over and out.
+        Started in the summer of 2019 by a band of misfits.
+      </p>
+      <p>
+        over and out.
       </p>
       </div>
 
