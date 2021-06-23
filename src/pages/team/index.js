ef1bfcaa2a36aa7f9a3da106e7ee316f56c9cd91
mtc-20 <mamenthomas.c@gmail.com>
2021-06-23 12:30:48 +0200

    get member details from json file

--- src/pages/team/index.js
+++ src/pages/team/index.js
@@ -1,52 +1,61 @@
 import React, { useState } from 'react';
 import Layout from '@theme/Layout';
 import styles from "./index.module.css"
-
+import data from "../../components/data/team.json"
 
 
 
 const ContactCard = props => {
   return(
     <div className={styles.contactcard}>
-      <img src={props.avatar} alt='profile' style={{width:'20vh'}}/>
+      <img src={props.avatar_url} alt='profile' style={{width:'20vh'}}/>
       <p>{props.name}</p>
-      <p><a href={'https://github.com/'+props.github} target='_blank'>@{props.github}</a></p>
+      <p><a href={'https://github.com/'+props.login} target='_blank'>@{props.login}</a></p>
     </div>
   );
 };
 
 // Just sample data
-const contacts = [
-  {name: 'Mamen Thomas Chembakasseril', github:'mtc-20', ghlink:'https://github.com/mtc-20', avatar:'https://avatars.githubusercontent.com/u/50070186?v=4'},
-  {name: 'Quang', github:'quangnhat185',avatar:'https://avatars.githubusercontent.com/u/41325216?v=4'},
-  {name: 'ABD', github:'hsrwrobotics'},
-  {name: 'ABD', github:'hsrwrobotics'},
-  {name: 'ABD', github:'hsrwrobotics'},
-  {name: 'ABD', github:'hsrwrobotics'},
-  {name: 'ABD', github:'hsrwrobotics'},
-  {name: 'ABD', github:'hsrwrobotics'},
-  {name: 'ABD', github:'hsrwrobotics'},
-  {name: 'ABD', github:'hsrwrobotics'},
-  {name: 'ABD', github:'hsrwrobotics'},
-  {name: 'ABD', github:'hsrwrobotics'},
-  {name: 'ABD', github:'hsrwrobotics'},
-  {name: 'ABD', github:'hsrwrobotics'},
-  {name: 'ABD', github:'hsrwrobotics'},
-  {name: 'ABD', github:'hsrwrobotics'},
-  {name: 'ABD', github:'hsrwrobotics'},
-  {name: 'ABD', github:'hsrwrobotics'},
-  {name: 'ABD', github:'hsrwrobotics'},
-  {name: 'ABD', github:'hsrwrobotics'},
-  {name: 'ABD', github:'hsrwrobotics'},
-  {name: 'ABD', github:'hsrwrobotics'},
-  {name: 'ABD', github:'hsrwrobotics'},
+// const contacts = [
+//   {name: 'Mamen Thomas Chembakasseril', login:'mtc-20', avatar_url:'https://avatars.githubusercontent.com/u/50070186?v=4'},
+//   {name: 'Quang', login:'quangnhat185', avatar_url:'https://avatars.githubusercontent.com/u/41325216?v=4'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
+//   {name: 'ABD', login:'hsrwrobotics'},
 
-]
+// ]
 
+// const cotacts = Array.from(data).map(contact=>{})
 
+// fetch(".\team.json")
+// .then(res=>{return res.json();})
+// .then(res=>{console.log(res)})
+// .catch(err=>{console.error(err)})
 
 
 function Hello() {
+  // const { siteConfig } = useDocusaurusContext();
 
   
   return (
@@ -67,11 +76,11 @@ function Hello() {
       <h1>The Team</h1>
       <div className={styles.cardContainer}>
 
-      {contacts.map(contact=>(
+      {data.map(contact=>(
         <ContactCard
-        avatar = {contact.avatar}
+        avatar_url = {contact.avatar_url}
         name = {contact.name}
-        github={contact.github}
+        login = {contact.login}
         />
         ))}
       </div>
