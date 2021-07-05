30aea1bb49a7366b4a5d7cfada14c72e7ba8065f
mtc-20 <mamenthomas.c@gmail.com>
2021-07-05 16:23:14 +0200

    dynamic linking to GitHub

--- src/pages/team/index.js
+++ src/pages/team/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import styles from "./index.module.css"
-import data from "../../components/data/team.json"
+// import team from "../../components/data/team.json"
+import teamIds from "../../components/data/teamIDs.json"
 
 
 
@@ -16,8 +17,34 @@ const ContactCard = props => {
 };
 
 
+function getDets2(username) {
+  const xhr = new XMLHttpRequest();
+  const url = `https://api.github.com/users/${username}`;
+  xhr.open('GET', url, false);
+  // xhr.onload = function() {
+    //   let data = JSON.parse(this.response)
+    //   console.log('name- ', data)
+    //   return data
+    // }
+    xhr.send()
+    let data = JSON.parse(xhr.responseText)
+    // console.log(data)
+    return data
+
+}
+
 
 function Hello() {
+  
+  let teamDets = []
+  for (let name of teamIds){
+    // console.log(name)
+    teamDets.push(getDets2(name))
+    // console.log(teamDets)  
+
+  }
+  // console.log(teamDets[1])
+  // teamDets.forEach(console.log)
 
   return (
     <Layout title="HSRW Robotics Club">
@@ -42,7 +69,7 @@ function Hello() {
       <h1>The Team</h1>
       <div className={styles.cardContainer}>
 
-      {data.map(contact=>(
+      {teamDets.map(contact=>(
         <ContactCard
         avatar_url = {contact.avatar_url}
         name = {contact.name}
