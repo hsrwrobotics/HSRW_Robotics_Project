aa3b25beaa248c3b0f48e2ddb322e98f558fd27b
mtc-20 <mamenthomas.c@gmail.com>
2021-07-06 12:11:12 +0200

    went back to original way of reading details from json file

--- src/pages/team/index.js
+++ src/pages/team/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import styles from "./index.module.css"
-// import team from "../../components/data/team.json"
-import teamIds from "../../components/data/teamIDs.json"
-
+import team from "../../components/data/team.json"
 
 
 const ContactCard = props => {
@@ -17,34 +15,7 @@ const ContactCard = props => {
 };
 
 
-function getDets2(username) {
-  const xhr = new XMLHttpRequest();
-  const url = `https://api.github.com/users/${username}`;
-  xhr.open('GET', url, false);
-  // xhr.onload = function() {
-    //   let data = JSON.parse(this.response)
-    //   console.log('name- ', data)
-    //   return data
-    // }
-    xhr.send()
-    let data = JSON.parse(xhr.responseText)
-    // console.log(data)
-    return data
-
-}
-
-
 function Hello() {
-  
-  let teamDets = []
-  for (let name of teamIds){
-    // console.log(name)
-    teamDets.push(getDets2(name))
-    // console.log(teamDets)  
-
-  }
-  // console.log(teamDets[1])
-  // teamDets.forEach(console.log)
 
   return (
     <Layout title="HSRW Robotics Club">
@@ -62,7 +33,7 @@ function Hello() {
       <br></br>
       <div className={styles.cardContainer}>
 
-      {teamDets.map(contact=>(
+      {team.map(contact=>(
         <ContactCard
         avatar_url = {contact.avatar_url}
         name = {contact.name}
